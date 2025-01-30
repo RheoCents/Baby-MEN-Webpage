69892b0ba9fa8c15238c1544fb101184defaa02d
ZeBonanaMan <150513668+ZeBonanaMan@users.noreply.github.com>
2025-01-30 18:41:37 +0800

    hehe

--- labexercise/Sorthing.py
+++ labexercise/Sorthing.py
@@ -1,73 +1,15 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        swapped = False
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
-                swapped = True
-        if not swapped:
-            break
-    return arr 
-
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]  
-    return arr
+class Sortingsort():
+    def __init__(self):
+        self.list = []
         
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j] :
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2 
-        L = arr[:mid]  
-        R = arr[mid:]
-
-        merge_sort(L)  
-        merge_sort(R)  
-        i = j = k = 0
-
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
-
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
-
-def quicksort(arr):
-    if len(arr) < 2:  
-        return arr
-    else:
-        pivot = arr[0]  
-        less = [i for i in arr[1:] if i <= pivot] 
-        greater = [i for i in arr[1:] if i > pivot] 
-        return quicksort(less) + [pivot] + quicksort(greater) 
-
-
-arr = [64, 34, 25, 12, 22, 11, 90]  
-ar = bubble_sort(arr.copy())  
-print("Selection Sort:", ar)
+    def bubble_sort(self):
+        for i in range(len(self.list)-1):
+            print(self.list)
+            for j in range(len(self.list)):
+                if j == (len(self.list)-1):
+                    continue
+                elif self.list[j] > self.list[j+1]:
+                    self.list[j], self.list[j+1] = self.list[j+1], self.list[j]
+                j+=1
+        return self.list
+    
